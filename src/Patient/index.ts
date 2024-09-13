a85a61b6f0df4631b46c6e30329dc836463264e0
Matheus <matheus.mpf18@gmail.com>
2024-09-13 08:55:58 -0300

    Criando função de separar os dados do rg com o orgao_emissor

--- src/Patient/index.ts
+++ src/Patient/index.ts
@@ -2,6 +2,9 @@ import { PrismaClient } from "@prisma/client";
 import * as XLSX from "xlsx";
 import path from "path";
 import moment from "moment";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const prisma = new PrismaClient();
 
@@ -24,7 +27,7 @@ type IData = {
   state: string;
   emissor: string;
   AddressComplement: string;
-  unidade_id: 91;
+  unidade_id: number;
 };
 
 // Função para renomear colunas
@@ -39,8 +42,17 @@ function renameColumns(data: any[], renameMap: Record<string, string>): any[] {
   });
 }
 
+// Função para separar RG e órgão emissor
+function separarRgEOrgao(rgCompleto: string) {
+  rgCompleto = rgCompleto.trim();
+  const partes = rgCompleto.split(" ");
+  const rg = partes.slice(0, -1).join(" ");
+  const orgao_emissor = partes[partes.length - 1];
+  return { rg, orgao_emissor };
+}
+
 // Ler o arquivo Excel
-const filePath = path.join(__dirname, 'Patient.xlsx');
+const filePath = path.join(__dirname, "Patient.xlsx");
 const workbook = XLSX.readFile(filePath);
 
 // Selecionar a primeira planilha
@@ -52,64 +64,67 @@ const data: any[] = XLSX.utils.sheet_to_json(worksheet);
 
 // Definir o mapa de renomeação de colunas
 const renameMap: Record<string, string> = {
-  Address: 'logradouro',
-  AddressNumber: 'numero',
-  BirthDate: 'nascimento',
-  CEP: 'cep',
-  City: 'cidade',
-  CivilStatus: 'estado_civil',
-  CreatedAt: 'createdAt',
-  DocumentId: 'rg',
-  MobilePhone: 'telefone',
-  Name: 'nome',
-  Neighborhood: 'bairro',
-  otherDocumentId: 'cpf',
-  otherPhones: 'numero2',
-  profession: 'profissao',
-  Sex: 'genero',
-  state: 'estado',
-  emissor: 'orgao_emissor',
-  AddressComplement: 'complemento',
-  unidade_id: 'unidade_id'
+  Address: "logradouro",
+  AddressNumber: "numero",
+  BirthDate: "nascimento",
+  CEP: "cep",
+  City: "cidade",
+  CivilStatus: "estado_civil",
+  CreatedAt: "createdAt",
+  DocumentId: "rg",
+  MobilePhone: "telefone",
+  Name: "nome",
+  Neighborhood: "bairro",
+  otherDocumentId: "cpf",
+  otherPhones: "numero2",
+  profession: "profissao",
+  Sex: "genero",
+  state: "estado",
+  emissor: "orgao_emissor",
+  AddressComplement: "complemento",
+  unidade_id: "unidade_id",
 };
 
 // Renomear as colunas
 const renamedData = renameColumns(data, renameMap);
 
+// Processamento de dados
+const results = renamedData.map((row: any) => {
+  // Separar RG e órgão emissor
+  const { rg, orgao_emissor } = separarRgEOrgao(row.rg || "");
+  console.log({ rg: rg });
 
-// // Processar os dados para garantir que estejam no formato esperado
-const results = renamedData.map((row: any) => ({
-  nome: row.nome || '',
-  nascimento: row.nascimento ? moment(row.nascimento, "DD/MM/YYYY").toDate() : null,
-  unidade_id: row.unidade_id || 0,
-  bairro: row.bairro || '',
-  cep: row.cep || '',
-  cidade: row.cidade || '',
-  cpf: row.cpf || '',
-  numero: row.numero || '',
-  logradouro: row.logradouro || '',
-  estado: row.estado || '',
-  complemento: row.complemento || '',
-  rg: row.rg || '',
-  orgao_emissor: row.orgao_emissor || '',
-  genero: row.genero || '',
-  telefone: row.telefone || '',
-  telefone2: row.numero2 || '',
-  profissao: row.profissao || '',
-}));
+  return {
+    nome: row.nome || "",
+    nascimento: row.nascimento
+      ? moment(row.nascimento, "YYYY-MM-DD").toDate()
+      : null,
+    unidade_id: row.unidade_id || 91,
+    bairro: row.bairro || "",
+    cep: row.cep || "",
+    cidade: row.cidade || "",
+    cpf: row.cpf || "",
+    numero: row.numero || "",
+    logradouro: row.logradouro || "",
+    estado: row.estado || "",
+    complemento: row.complemento || "",
+    rg,
+    orgao_emissor,
+    genero: row.genero || "",
+    telefone: row.telefone || "",
+    telefone2: row.numero2 || "",
+    profissao: row.profissao || "",
+  };
+});
 
 // Salvar os dados no banco de dados
 (async () => {
   try {
     await prisma.pacientes.createMany({
-      data: results
+      data: results,
     });
-    console.log('Dados salvos com sucesso!');
+    console.log("Dados salvos com sucesso!");
   } catch (error) {
-    console.error('Erro ao salvar dados:', error);
-  } finally {
-    await prisma.$disconnect();
+    console.error("Erro ao salvar dados:", error);
   }
 })();
-
-console.log({renamed: results[0]})
